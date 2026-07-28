4ebc44f7195c50fc8b82dd43ebe8e218aa890f9a
Illia Lotfalian <122395038+illia1383@users.noreply.github.com>
2026-07-27 20:06:51 -0400

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        #A validTree has no cycles and all the edges are connected 
+        # edges > n - 1 return false
+
+        if len(edges) > n - 1:
+            return False 
+
+        adj = [[] for _ in range(n)]
+        
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        visit = set()
+
+        q = deque([(0, -1)])  # (current node, parent node)
+
+        visit.add(0)
+
+        while q:
+            node, parent = q.popleft()
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if nei in visit:
+                    return False 
+                visit.add(nei)
+                q.append((nei,node)) #current node, parent 
+
+        return len(visit) == n
